0f32ed702cbf3ef9af941004eb77fea3d3d5bb80
letfontenellec <letfontenellec@protonmail.com>
2024-10-07 21:27:50 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PomodoroTimer from './components/PomodoroTimer';
+import PomodoroTimer from './components/PomodoroTimer'
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// Importando os hooks necessários do React
-import { useState, useEffect } from 'react'
-// Importando componentes UI personalizados
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Switch } from "@/components/ui/switch"
-// Importando ícones do Lucide
-import { PlayIcon, PauseIcon, FlagIcon, RefreshCwIcon, ArrowLeftIcon } from 'lucide-react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
